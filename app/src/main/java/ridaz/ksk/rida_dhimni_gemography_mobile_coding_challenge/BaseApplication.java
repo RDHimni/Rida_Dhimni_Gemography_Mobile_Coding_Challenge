864abf7c373554881f874a0565c69438b6c0fc14
Rida Dhimni <rdhimni@gmail.com>
2021-05-21 23:19:40 +0200

    Divide the project according to MvvM architecture pattern and setup DaggerHilt.

---
+++ app/src/main/java/ridaz/ksk/rida_dhimni_gemography_mobile_coding_challenge/BaseApplication.java
@@ -0,0 +1,14 @@
+package ridaz.ksk.rida_dhimni_gemography_mobile_coding_challenge;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+/**
+ * Rida Dhimni
+ * 22/03/2021
+ **/
+
+@HiltAndroidApp
+public class BaseApplication extends Application {
+}
