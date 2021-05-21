ec34f1a3d7d515d822e08a46a7b4fbdfafd44b70
Rida Dhimni <rdhimni@gmail.com>
2021-05-21 21:36:27 +0200

    Initial commit

---
+++ app/src/main/java/ridaz/ksk/rida_dhimni_gemography_mobile_coding_challenge/MainActivity.java
@@ -0,0 +1,14 @@
+package ridaz.ksk.rida_dhimni_gemography_mobile_coding_challenge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/ridaz/ksk/rida_dhimni_gemography_mobile_coding_challenge/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package ridaz.ksk.rida_dhimni_gemography_mobile_coding_challenge;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
